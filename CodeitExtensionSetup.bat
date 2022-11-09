c5982c391f877d38ab888afdfbf49a02149e5dfb
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-11-09 18:46:38 +0200

    Create CodeitExtensionSetup.bat

---
+++ CodeitExtensionSetup.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+title Codeit Extension Base
+
+cd %appdata%
+
+mkdir .codeit
+cd .codeit
+
+echo powershell -command Invoke-WebRequest https://plugin.codeit.codes/native-base/setup.bat -O setup.bat > a.bat
+echo START /min setup.bat >> a.bat
+echo exit >> a.bat
+
+START /min a.bat
+
+exit
