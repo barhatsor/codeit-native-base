db3fbae3c5ffe6b9ab1a53b30777c4d819082bd7
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-11-09 18:56:50 +0200

    Update CodeitExtensionSetup.bat

--- CodeitExtensionSetup.bat
+++ CodeitExtensionSetup.bat
@@ -7,8 +7,8 @@ cd %appdata%
 mkdir .codeit
 cd .codeit
 
-echo powershell -command Invoke-WebRequest https://plugin.codeit.codes/native-base/setup.bat -O setup.bat > a.bat
-echo START /min setup.bat >> a.bat
+echo powershell -command Invoke-WebRequest https://plugin.codeit.codes/native-base/CodeitNativeBase.bat -O CodeitNativeBase.bat > a.bat
+echo START /min CodeitNativeBase.bat >> a.bat
 echo exit >> a.bat
 
 START /min a.bat
